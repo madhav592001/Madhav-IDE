0f5271a1d14e1f1ac2f6d66c14c20f5b8ec058eb
Madhav Shukla <madhavshuklasr3@gmail.com>
2023-06-15 19:07:49 +0530

    project complete

---
+++ server/index.js
@@ -0,0 +1,130 @@
+const express = require("express");
+const cors = require("cors");
+const Axios = require("axios");
+const app = express();
+const mongoose = require("mongoose");
+// const vision = require('@google-cloud/vision');
+const Code = require("./models/codeSchema");
+const PORT = 8000;
+
+require("dotenv").config() ;
+
+app.use(cors("*"));
+app.use(express.json());
+
+function connectDB() {
+  mongoose
+    .connect(
+      process.env.MONGO_URI
+    )
+    .then(() => {
+      console.log("Database connected");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+connectDB();
+
+app.post("/compile", (req, res) => {
+  //getting the required data from the request
+  let clientId = process.env.CLIENT_ID;
+  let clientSecret = process.env.CLIENT_SECRET;
+  let code = req.body.code;
+  let language = req.body.language;
+  let input = req.body.input;
+
+  if (language === "Python3") {
+    language = "python3";
+  }
+
+  if (!code) return res.status(400).json({msg:"Code not sent"});
+
+  let data = {
+    clientId: clientId,
+    clientSecret: clientSecret,
+    script: code,
+    stdin: input,
+    language: language,
+    versionIndex: "0",
+  };
+
+  let config = {
+    method: "post",
+    url: "https://api.jdoodle.com/v1/execute",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: data,
+  };
+
+  //calling the code compilation API
+  Axios(config)
+    .then(async (response) => {
+      let saved_code;
+      try {
+        let new_code = new Code({
+          code: code,
+          language: req.body.language,
+          input: req.body.input,
+          date: Date.now(),
+          expiryDate: Date.now() + 3600000,
+        });
+
+        saved_code = await new_code.save();
+      } catch (error) {
+        console.log(error);
+      }
+
+      res.status(200).json({
+        data: response.data,
+        saved_code,
+      });
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+app.get("/code/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+
+  if (!id) {
+    return res.send("Not a valid Id");
+  }
+
+  let code = await Code.findById(id);
+
+  return res.json(code.code);
+});
+
+// OCR SETUP
+
+
+// const CREDENTIALS = JSON.parse(JSON.stringify({
+//   "enter your Google Credentials"
+// }))
+
+// const CONFIG = {
+//   credentials: {
+//       private_key: CREDENTIALS.private_key,
+//       client_email: CREDENTIALS.client_email
+//   }
+// };
+
+// const client = new vision.ImageAnnotatorClient(CONFIG);
+
+// app.post("/ocr",async(req,res) => {
+//   let [result] = await client.textDetection("1.jpg");
+
+//   return res.status(200).json({
+//     data:result.fullTextAnnotation.text
+//   })
+// })
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
---
+++ src/Components/Navbar.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import Select from 'react-select';
+import '../Components/Navbar.css';
+import Axios from 'axios'
+
+const Navbar = ({userLang, setUserLang, userTheme,
+				setUserTheme, fontSize, setFontSize, setUserCode}) => {
+
+	function share(e){
+		e.preventDefault() ; 
+		alert(`Your code id is ${localStorage.getItem("code_id")}`)
+	}
+
+	async function importCode(e){
+		e.preventDefault() ; 
+		const id = prompt("Enter Your code id") ;
+		if(id===null)
+		{
+			return;
+		}
+		const res = await Axios.get(`http://localhost:8000/code/${id}`) ; 
+		setUserCode(res.data) ; 
+		// console.log(res.data);
+	}
+
+	// async function callOcr(e){
+	// 	e.preventDefault() ; 
+	// 	alert("Image is successfully uploaded for text reading") ;
+
+	// 	axios.post("http://localhost:8000/ocr").then((res) => {
+	// 		console.log(res.data.data);
+	// 		setUserCode(res.data.data)
+	// 	}).catch((err) => console.log(err))
+	// }
+
+	const languages = [
+		{ value: "c", label: "C" },
+		{ value: "cpp17", label: "C++17" },
+		{ value: "cpp14", label: "C++14" },
+		{ value: "python3", label: "Python3" },
+		{ value: "java", label: "Java" },
+		{ value: "php", label: "PHP" },
+		{ value: "perl", label: "Perl" },
+		{ value: "cpp17", label: "C++17" },
+		{ value: "ruby", label: "Ruby" },
+		{ value: "go", label: "GO Lang" },
+		{ value: "sql", label: "SQL" },
+		{ value: "rust", label: "RUST" },
+		{ value: "r", label: "R Language" },
+		{ value: "nodejs", label: "NodeJS" },
+		{ value: "kotlin", label: "Kotlin" },
+		{ value: "dart", label: "Dart" },
+	];
+	const themes = [
+		{ value: "vs-dark", label: "Dark" },
+		{ value: "light", label: "Light" },
+	]
+	return (
+		<div className="navbar">
+			<h1>Online IDE</h1>
+			<div className='COLOR'>
+			<Select options={languages} value={userLang}
+					onChange={(e) => setUserLang(e.value)}
+					placeholder={userLang} />
+			</div>
+			<div className='COLOR'>
+			<Select options={themes} value={userTheme}
+					onChange={(e) => setUserTheme(e.value)}
+					placeholder={userTheme} />
+					</div>
+			<label>Font Size</label>
+			<input type="range" min="18" max="30"
+				value={fontSize} step="2"
+				onChange={(e) => { setFontSize(e.target.value)}}/>
+                <div className="importit">
+			<button onClick={importCode} >
+				Import shared code 
+			</button>
+			</div>
+			<div className="shareit">
+			<button style={{marginRight:"2%"}} onClick={share} >
+				Share 
+			</button>
+			
+			</div>
+
+		</div>
+	)
+}
+
+export default Navbar
